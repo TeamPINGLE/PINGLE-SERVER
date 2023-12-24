0e16568a2ab4b153fcc1ac8546401d52240643f6
Parkjyun <jaeyun0507@naver.com>
2023-12-24 22:36:20 +0900

    [init] 프로젝트 초기 세팅

---
+++ src/main/java/org/pingle/pingleserver/PingleserverApplication.java
@@ -0,0 +1,13 @@
+package org.pingle.pingleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingleserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingleserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pingle/pingleserver/PingleserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pingle.pingleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingleserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
