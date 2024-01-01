479f82525b6989abc96581bd82607ea032a14708
tkdwns414 <sangjune2000@naver.com>
2024-01-02 05:06:20 +0900

    [feat] create ApiResponse

---
+++ src/main/java/org/pingle/pingleserver/dto/common/ApiResponse.java
@@ -0,0 +1,32 @@
+package org.pingle.pingleserver.dto.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.pingle.pingleserver.dto.type.ErrorMessage;
+import org.pingle.pingleserver.dto.type.SuccessMessage;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T>{
+
+    private final int code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static <T> ApiResponse<T> success(SuccessMessage success) {
+        return new ApiResponse<>(success.getHttpStatusCode(), success.getMessage());
+    }
+
+    public static <T> ApiResponse<T> success(SuccessMessage success, T data) {
+        return new ApiResponse<>(success.getHttpStatusCode(), success.getMessage(), data);
+    }
+
+    public static <T> ApiResponse<T> fail(ErrorMessage fail) {
+        return new ApiResponse<>(fail.getHttpStatusCode(), fail.getMessage());
+    }
+}
