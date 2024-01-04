8c238b6d44eb98387edb61969246d09731069783
Parkjyun <jaeyun0507@naver.com>
2024-01-05 01:50:59 +0900

    [feat] create UserTeam, UserTeamService, UserTeamRepository

---
+++ src/main/java/org/pingle/pingleserver/domain/UserTeam.java
@@ -0,0 +1,28 @@
+package org.pingle.pingleserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.pingle.pingleserver.domain.enums.TRole;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserTeam extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @Enumerated(EnumType.STRING)
+    private TRole teamRole;
+}
---
+++ src/main/java/org/pingle/pingleserver/domain/enums/TRole.java
@@ -0,0 +1,13 @@
+package org.pingle.pingleserver.domain.enums;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum TRole {
+    TOWNER("owner"), TPARTICIPANT("participant");
+
+    private final String value;
+}
