10e15ae6ae432400c7bc38fd7d0421293ef9ce4e
Parkjyun <jaeyun0507@naver.com>
2024-01-05 01:48:31 +0900

    [feat] create Pin, PinService, PinRepository

---
+++ src/main/java/org/pingle/pingleserver/domain/Point.java
@@ -0,0 +1,18 @@
+package org.pingle.pingleserver.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Point {
+
+    private Double x;
+
+    private Double y;
+}
