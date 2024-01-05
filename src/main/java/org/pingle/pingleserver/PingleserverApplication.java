8fe992b72893762ae34f2a2e02d1f68ff65faa8d
tkdwns414 <sangjune2000@naver.com>
2024-01-06 02:58:30 +0900

    [feat] spring security for JWT Authentication

--- src/main/java/org/pingle/pingleserver/PingleserverApplication.java
+++ src/main/java/org/pingle/pingleserver/PingleserverApplication.java
@@ -2,8 +2,9 @@ package org.pingle.pingleserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class PingleserverApplication {
 
 	public static void main(String[] args) {
