83d83a12371924f249793284ea46b324af27e017
tkdwns414 <sangjune2000@naver.com>
2024-01-06 16:10:55 +0900

    [feat] add name field in dto for apple login

--- src/main/java/org/pingle/pingleserver/dto/request/LoginRequest.java
+++ src/main/java/org/pingle/pingleserver/dto/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package org.pingle.pingleserver.dto.request;
 
+import lombok.NonNull;
 import org.pingle.pingleserver.domain.enums.Provider;
 
-public record LoginRequest(Provider provider) {
+public record LoginRequest(@NonNull Provider provider, String name) {
 }
