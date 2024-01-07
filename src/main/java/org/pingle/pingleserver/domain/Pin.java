95c2a04f3d6b7c200f0dda056429faadd27bf637
Parkjyun <jaeyun0507@naver.com>
2024-01-07 23:43:48 +0900

    [fix] edit entity for create meeting api

--- src/main/java/org/pingle/pingleserver/domain/Pin.java
+++ src/main/java/org/pingle/pingleserver/domain/Pin.java
@@ -2,6 +2,7 @@ package org.pingle.pingleserver.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,4 +25,12 @@ public class Pin extends BaseTimeEntity {
     private Address address;
 
     private String name;
+
+    @Builder
+    public Pin(Team team, Point point, Address address, String name) {
+        this.team = team;
+        this.point = point;
+        this.address = address;
+        this.name = name;
+    }
 }
