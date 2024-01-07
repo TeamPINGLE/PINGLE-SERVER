fa8e44750c2e9034357535245725532818ff934f
Parkjyun <jaeyun0507@naver.com>
2024-01-07 23:48:02 +0900

    [feat] add methods in service for create meeting api

--- src/main/java/org/pingle/pingleserver/service/MeetingService.java
+++ src/main/java/org/pingle/pingleserver/service/MeetingService.java
@@ -1,11 +1,35 @@
 package org.pingle.pingleserver.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.pingle.pingleserver.domain.Meeting;
+import org.pingle.pingleserver.domain.Pin;
+import org.pingle.pingleserver.dto.request.MeetingRequest;
+import org.pingle.pingleserver.repository.MeetingRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MeetingService {
+
+
+    private final MeetingRepository meetingRepository;
+
+    @Transactional
+    public Meeting createMeeting(MeetingRequest request, Pin pin) {
+        return meetingRepository.save(
+                Meeting.builder()
+                        .pin(pin)
+                        .category(request.category())
+                        .name(request.name())
+                        .maxParticipants(request.maxParticipants())
+                        .chatLink(request.chatLink())
+                        .startAt(request.startAt())
+                        .endAt(request.endAt())
+                        .build());
+
+    }
 }
--- src/main/java/org/pingle/pingleserver/service/UserMeetingService.java
+++ src/main/java/org/pingle/pingleserver/service/UserMeetingService.java
@@ -1,6 +1,14 @@
 package org.pingle.pingleserver.service;
 
 import lombok.RequiredArgsConstructor;
+import org.pingle.pingleserver.domain.Meeting;
+import org.pingle.pingleserver.domain.Team;
+import org.pingle.pingleserver.domain.User;
+import org.pingle.pingleserver.domain.UserMeeting;
+import org.pingle.pingleserver.domain.enums.MRole;
+import org.pingle.pingleserver.dto.type.ErrorMessage;
+import org.pingle.pingleserver.exception.BusinessException;
+import org.pingle.pingleserver.repository.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +16,28 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserMeetingService {
+    private final UserRepository userRepository;
+    private final TeamRepository teamRepository;
+    private final UserMeetingRepository userMeetingRepository;
+    private final UserTeamRepository userTeamRepository;
+
+    @Transactional
+    public Long addOwnerToMeeting(Long userId, Meeting meeting) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new BusinessException(ErrorMessage.NO_SUCH_USER));
+        return userMeetingRepository.save(
+                UserMeeting.builder()
+                        .user(user)
+                        .meeting(meeting)
+                        .meetingRole(MRole.OWNER)
+                        .build()).getId();
+    }
+
+    //유저가 그룹에 있는지
+    public void verifyUser(Long userId, Long groupId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new BusinessException(ErrorMessage.NO_SUCH_USER));
+        Team team = teamRepository.findById(groupId).orElseThrow(() -> new BusinessException(ErrorMessage.RESOURCE_NOT_FOUND));
+        userTeamRepository.findByUserAndTeam(user, team).orElseThrow(() -> new BusinessException(ErrorMessage.GROUP_PERMISSION_DENIED));
+
+
+    }
 }
