967da770d3e85d6a08ee829cb584066d27d2ee16
Parkjyun <jaeyun0507@naver.com>
2024-01-08 04:58:48 +0900

    [feat] create logic for meetingList api

--- src/main/java/org/pingle/pingleserver/repository/UserMeetingRepository.java
+++ src/main/java/org/pingle/pingleserver/repository/UserMeetingRepository.java
@@ -1,7 +1,15 @@
 package org.pingle.pingleserver.repository;
 
+import org.pingle.pingleserver.domain.Meeting;
 import org.pingle.pingleserver.domain.UserMeeting;
+import org.pingle.pingleserver.domain.enums.MRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserMeetingRepository extends JpaRepository<UserMeeting, Long> {
+    Optional<UserMeeting> findByMeetingAndMeetingRole(Meeting meeting, MRole role);
+    boolean existsByUserIdAndMeeting(Long userId, Meeting meeting);
+    List<UserMeeting> findAllByMeeting(Meeting meeting);
 }
