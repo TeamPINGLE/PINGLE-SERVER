a04631d74d30e02cd5d35020fd1dbd0fb6e9bafd
Parkjyun <jaeyun0507@naver.com>
2024-01-07 23:46:35 +0900

    [feat] add methods in repository for create meeting api

--- src/main/java/org/pingle/pingleserver/repository/PinRepository.java
+++ src/main/java/org/pingle/pingleserver/repository/PinRepository.java
@@ -1,7 +1,10 @@
 package org.pingle.pingleserver.repository;
 
 import org.pingle.pingleserver.domain.Pin;
+import org.pingle.pingleserver.domain.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PinRepository extends JpaRepository<Pin, Long> {
+    boolean existsByPoint(Point point);
+    Pin findByPoint(Point point);
 }
--- src/main/java/org/pingle/pingleserver/repository/UserMeetingRepository.java
+++ src/main/java/org/pingle/pingleserver/repository/UserMeetingRepository.java
@@ -3,5 +3,7 @@ package org.pingle.pingleserver.repository;
 import org.pingle.pingleserver.domain.UserMeeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface UserMeetingRepository extends JpaRepository<UserMeeting, Long> {
 }
