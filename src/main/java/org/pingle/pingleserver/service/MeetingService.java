38439fc9c88720edabd66fb0a4740953db5bdd29
Parkjyun <jaeyun0507@naver.com>
2024-01-15 02:26:57 +0900

    [feat] crate logic for get meeting participants api

--- src/main/java/org/pingle/pingleserver/service/MeetingService.java
+++ src/main/java/org/pingle/pingleserver/service/MeetingService.java
@@ -3,10 +3,17 @@ package org.pingle.pingleserver.service;
 import lombok.RequiredArgsConstructor;
 import org.pingle.pingleserver.domain.Meeting;
 import org.pingle.pingleserver.domain.Pin;
+import org.pingle.pingleserver.domain.UserMeeting;
 import org.pingle.pingleserver.dto.request.MeetingRequest;
+import org.pingle.pingleserver.dto.response.ParticipantsResponse;
+import org.pingle.pingleserver.dto.type.ErrorMessage;
+import org.pingle.pingleserver.exception.CustomException;
 import org.pingle.pingleserver.repository.MeetingRepository;
+import org.pingle.pingleserver.repository.UserMeetingRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MeetingService {
 
     private final MeetingRepository meetingRepository;
+    private final UserMeetingRepository userMeetingRepository;
 
     @Transactional
     public Meeting createMeeting(MeetingRequest request, Pin pin) {
@@ -28,5 +36,12 @@ public class MeetingService {
                         .endAt(request.endAt())
                         .build());
 
+    }
+
+    public ParticipantsResponse getParticipants(Long meetingId) {
+        Meeting meeting = meetingRepository.findById(meetingId)
+                .orElseThrow(() -> new CustomException(ErrorMessage.RESOURCE_NOT_FOUND));
+        List<UserMeeting> userMeetings = userMeetingRepository.findAllByMeeting(meeting);
+        return ParticipantsResponse.of(userMeetings);
     }
 }
