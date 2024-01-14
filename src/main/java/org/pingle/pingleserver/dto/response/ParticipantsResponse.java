8fcd6056c494dae0595cb3d7eb0853d0498fb626
Parkjyun <jaeyun0507@naver.com>
2024-01-15 02:27:53 +0900

    [feat] crate get meeting participants api

---
+++ src/main/java/org/pingle/pingleserver/dto/response/ParticipantsResponse.java
@@ -0,0 +1,16 @@
+package org.pingle.pingleserver.dto.response;
+
+import org.pingle.pingleserver.domain.UserMeeting;
+
+import java.util.Collections;
+import java.util.List;
+
+public record ParticipantsResponse(
+        List<String> participants
+) {
+    public static ParticipantsResponse of(List<UserMeeting> userMeetings) {
+        Collections.sort(userMeetings);
+        List<String> participantNames = userMeetings.stream().map(userMeeting -> userMeeting.getUser().getName()).toList();
+        return new ParticipantsResponse(participantNames);
+    }
+}
