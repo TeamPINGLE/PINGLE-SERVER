81e1a658449e16c78c81bcaba0b674b0774b45e4
Parkjyun <jaeyun0507@naver.com>
2024-02-22 21:30:08 +0900

    [fix] delete logic in PinResponse

--- src/main/java/org/pingle/pingleserver/dto/reponse/PinResponse.java
+++ src/main/java/org/pingle/pingleserver/dto/reponse/PinResponse.java
@@ -1,45 +1,6 @@
 package org.pingle.pingleserver.dto.reponse;
 
-import org.pingle.pingleserver.domain.Meeting;
-import org.pingle.pingleserver.domain.Pin;
 import org.pingle.pingleserver.domain.enums.MCategory;
 
-import java.util.Comparator;
-import java.util.List;
-
-public record PinResponse(Long id, Double x, Double y, MCategory category, int meetingCount) {
-    public static PinResponse of(Pin pin, MCategory category, int count) {
-        return new PinResponse(pin.getId(),pin.getPoint().getX(), pin.getPoint().getY(),
-                category, count);
-    }
-    public static PinResponse ofWithNoFilter(Pin pin) {
-        return new PinResponse(pin.getId(),pin.getPoint().getX(), pin.getPoint().getY(),
-                getMostRecentMeetingCategoryOfPin(pin), getMeetingCount(pin));
-    }
-    public static PinResponse ofWithFilter(Pin pin, MCategory mCategory) {
-        return new PinResponse(pin.getId(),pin.getPoint().getX(), pin.getPoint().getY(),
-                mCategory, getMeetingCountWithFilter(pin, mCategory));
-
-    }
-    private static MCategory getMostRecentMeetingCategoryOfPin (Pin pin) {
-        Comparator<Meeting> comparator = Comparator.comparing(Meeting::getStartAt);
-        List<Meeting> meetingList = pin.getMeetingList();
-        meetingList.sort(comparator);
-        return meetingList.get(0).getCategory();
-    }
-
-    private static int getMeetingCount(Pin pin) {
-        return pin.getMeetingList().size();
-    }
-    private static int getMeetingCountWithFilter(Pin pin, MCategory category) {
-        int count = 0;
-        List<Meeting> meetings = pin.getMeetingList();
-        for(Meeting meeting : meetings) {
-            if(meeting.getCategory().getValue().equals(category.getValue()))
-                count++;
-        }
-
-        return count;
-    }
-
+public record PinResponse(Long id, Double x, Double y, MCategory category) {
 }
